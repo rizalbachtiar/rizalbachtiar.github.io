30b05777c7ee0bdd90cdd54fea3aad9343e5a703
rizal bachtiar <rizal.bachtiar.31@gmail.com>
2021-11-25 00:22:59 +0700

    make view portfolio

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-28 py-10">
+  <div class="px-28 py-10 space-y-10">
     <div class="flex flex-row justify-between items-center">
       <img class="h-14" src="https://www.nicepng.com/png/full/380-3802664_know-the-content-writing-team-at-webinova-content.png">
       <ul class="flex flex-row space-x-5 text-gray-500 text-sm font-semibold">
@@ -9,17 +9,37 @@
         <li><a href="#">Contact</a></li>
       </ul>
     </div>
-    <div class="pt-28 flex flex-row items-center justify-between">
-      <div class="space-y-5 w-7/12 px-10">
+    <div class="pt-16 flex flex-row items-center justify-between">
+      <div class="space-y-5 w-7/12 pr-10">
         <div class="text-xl">Hi, my name is Rizal Bachtiar.</div>
         <div class="text-3xl">I'm a freelance UI/UX Designer with a marketing background specializing in Shopify & Webflow.</div>
         <div class="text-sm space-x-3">
           <button class="border border-yellow-400 rounded py-2 px-5 text-yellow-400 shadow">My Work</button>
-          <button class="rounded py-2 px-5 bg-gradient-to-r from-pink-500 to-yellow-500 shadow">Let's Talk</button>
+          <button class="rounded py-2 px-5 bg-gradient-to-r from-pink-500 to-yellow-500 shadow text-white">Let's Talk</button>
         </div>
       </div>
       <div class="w-5/12 flex justify-end">
         <img class="h-80" src="https://www.pngitem.com/pimgs/m/224-2247730_transparent-person-waving-png-cartoon-person-png-png.png">
+      </div>
+    </div>
+    <div class="space-y-10">
+      <div class="text-2xl font-semibold">What I do</div>
+      <div class="flex flex-row space-x-10">
+        <div class="w-4/12 space-y-3">
+          <img class="h-14" src="https://cdn-icons.flaticon.com/png/512/4334/premium/4334301.png?token=exp=1637774338~hmac=0283f2626a86a7da0022c44b51ca7885">
+          <div class="font-semibold">UI/UX Design</div>
+          <div class="text-sm text-gray-500">Need help with designing your website? I can create beautiful website designs based on your content. or redesign old websites to improve your conversions or achieve your business goals</div>
+        </div>
+        <div class="w-4/12 space-y-3">
+          <img class="h-14" src="https://cdn-icons-png.flaticon.com/512/2331/2331970.png">
+          <div class="font-semibold">Shopify Design & Setup</div>
+          <div class="text-sm text-gray-500">Need help with designing your website? I can create beautiful website designs based on your content. or redesign old websites to improve your conversions or achieve your business goals</div>
+        </div>
+        <div class="w-4/12 space-y-3">
+          <img class="h-14" src="https://cdn-icons.flaticon.com/png/512/2248/premium/2248656.png?token=exp=1637775237~hmac=a650c960269657840ed3c3a46f54e414">
+          <div class="font-semibold">Webflow Design & Setup</div>
+          <div class="text-sm text-gray-500">Need help with designing your website? I can create beautiful website designs based on your content. or redesign old websites to improve your conversions or achieve your business goals</div>
+        </div>
       </div>
     </div>
   </div>
