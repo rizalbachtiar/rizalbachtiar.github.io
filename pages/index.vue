7f82a4871beeb111581f53790b88a9a61f752a0d
rizalbachtiar <rizal.bachtiar.31@gmail.com>
2022-01-09 17:49:59 +0700

    make new profile

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,106 @@
 <template>
-  <div class="px-28 py-10 space-y-10">
-    <div class="flex flex-row justify-between items-center">
-      <img class="h-14" src="https://www.nicepng.com/png/full/380-3802664_know-the-content-writing-team-at-webinova-content.png">
-      <ul class="flex flex-row space-x-5 text-gray-500 text-sm font-semibold">
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </div>
-    <div class="pt-16 flex flex-row items-center justify-between">
-      <div class="space-y-5 w-7/12 pr-10">
-        <div class="text-xl">Hi, my name is Rizal Bachtiar.</div>
-        <div class="text-3xl">I'm a freelance UI/UX Designer with a marketing background specializing in Shopify & Webflow.</div>
-        <div class="text-sm space-x-3">
-          <button class="border border-yellow-400 rounded py-2 px-5 text-yellow-400 shadow">My Work</button>
-          <button class="rounded py-2 px-5 bg-gradient-to-r from-pink-500 to-yellow-500 shadow text-white">Let's Talk</button>
+  <div class="w-screen h-screen bg-cover object-center py-5" style="background-image: url('/background.jpg');">
+    <div class="max-w-4xl mx-auto">
+      <div class="flex justify-end">
+        <div class="w-4/12 -mr-1 flex justify-end">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-auto w-24" viewBox="0 0 136 70">
+            <path fill="#fff" d="M64 48.3333C40.3125 70 0 70 0 70h135.5V0h-8.673C104.41 0 85.7045 15.5842 74.3306 34.9015 71.281 40.0808 67.8169 44.8421 64 48.3333z"/>
+          </svg>
+        </div>
+        <div class="w-7/12 bg-white flex flex-col justify-center rounded-tr-3xl">
+          <ul class="flex justify-between pl-5 pr-10 pt-3">
+              <li class="text-gray-500 text-sm hover:text-blue-500"><a href="#">ABOUT</a></li>
+              <li class="text-gray-500 text-sm hover:text-blue-500"><a href="#">RESUME</a></li>
+              <li class="text-gray-500 text-sm hover:text-blue-500"><a href="#">PORTFOLIO</a></li>
+              <li class="text-gray-500 text-sm hover:text-blue-500"><a href="#">BLOG</a></li>
+          </ul>
         </div>
       </div>
-      <div class="w-5/12 flex justify-end">
-        <img class="h-80" src="https://www.pngitem.com/pimgs/m/224-2247730_transparent-person-waving-png-cartoon-person-png-png.png">
-      </div>
-    </div>
-    <div class="space-y-10">
-      <div class="text-2xl font-semibold">What I do</div>
-      <div class="flex flex-row space-x-10">
-        <div class="w-4/12 space-y-3">
-          <img class="h-14" src="https://cdn-icons.flaticon.com/png/512/4334/premium/4334301.png?token=exp=1637774338~hmac=0283f2626a86a7da0022c44b51ca7885">
-          <div class="font-semibold">UI/UX Design</div>
-          <div class="text-sm text-gray-500">Need help with designing your website? I can create beautiful website designs based on your content. or redesign old websites to improve your conversions or achieve your business goals</div>
-        </div>
-        <div class="w-4/12 space-y-3">
-          <img class="h-14" src="https://cdn-icons-png.flaticon.com/512/2331/2331970.png">
-          <div class="font-semibold">Shopify Design & Setup</div>
-          <div class="text-sm text-gray-500">Need help with designing your website? I can create beautiful website designs based on your content. or redesign old websites to improve your conversions or achieve your business goals</div>
-        </div>
-        <div class="w-4/12 space-y-3">
-          <img class="h-14" src="https://cdn-icons.flaticon.com/png/512/2248/premium/2248656.png?token=exp=1637775237~hmac=a650c960269657840ed3c3a46f54e414">
-          <div class="font-semibold">Webflow Design & Setup</div>
-          <div class="text-sm text-gray-500">Need help with designing your website? I can create beautiful website designs based on your content. or redesign old websites to improve your conversions or achieve your business goals</div>
+      <div class="bg-white rounded-tl-3xl rounded-b-3xl">
+        <div class="w-3/12 bg-gray-200 rounded-3xl py-8 px-5 flex flex-col items-center space-y-3">
+          <img class="w-36 h-36 rounded-3xl" src="/profile.jpeg" alt="profile">
+          <div class="text-gray-600 text-2xl font-serif">Rizal Bachtiar</div>
+          <div class="bg-gray-300 text-xs py-1 px-4 rounded-full text-gray-500">Junior Enginer</div>
+          <ul class="flex space-x-4 items-center">
+              <li>
+                <a href="#">
+                  <svg class="w-2" viewBox="0 0 10 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="Dribbble-Light-Preview" transform="translate(-385.000000, -7399.000000)" fill="#6B7280">
+                              <g id="icons" transform="translate(56.000000, 160.000000)">
+                                  <path d="M335.821282,7259 L335.821282,7250 L338.553693,7250 L339,7246 L335.821282,7246 L335.821282,7244.052 C335.821282,7243.022 335.847593,7242 337.286884,7242 L338.744689,7242 L338.744689,7239.14 C338.744689,7239.097 337.492497,7239 336.225687,7239 C333.580004,7239 331.923407,7240.657 331.923407,7243.7 L331.923407,7246 L329,7246 L329,7250 L331.923407,7250 L331.923407,7259 L335.821282,7259 Z" id="facebook-[#176]"></path>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <svg class="w-4" viewBox="0 0 20 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="Dribbble-Light-Preview" transform="translate(-60.000000, -7521.000000)" fill="#6B7280">
+                              <g id="icons" transform="translate(56.000000, 160.000000)">
+                                  <path d="M10.29,7377 C17.837,7377 21.965,7370.84365 21.965,7365.50546 C21.965,7365.33021 21.965,7365.15595 21.953,7364.98267 C22.756,7364.41163 23.449,7363.70276 24,7362.8915 C23.252,7363.21837 22.457,7363.433 21.644,7363.52751 C22.5,7363.02244 23.141,7362.2289 23.448,7361.2926 C22.642,7361.76321 21.761,7362.095 20.842,7362.27321 C19.288,7360.64674 16.689,7360.56798 15.036,7362.09796 C13.971,7363.08447 13.518,7364.55538 13.849,7365.95835 C10.55,7365.79492 7.476,7364.261 5.392,7361.73762 C4.303,7363.58363 4.86,7365.94457 6.663,7367.12996 C6.01,7367.11125 5.371,7366.93797 4.8,7366.62489 L4.8,7366.67608 C4.801,7368.5989 6.178,7370.2549 8.092,7370.63591 C7.488,7370.79836 6.854,7370.82199 6.24,7370.70483 C6.777,7372.35099 8.318,7373.47829 10.073,7373.51078 C8.62,7374.63513 6.825,7375.24554 4.977,7375.24358 C4.651,7375.24259 4.325,7375.22388 4,7375.18549 C5.877,7376.37088 8.06,7377 10.29,7376.99705" id="twitter-[#154]"></path>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <svg class="w-4" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="Dribbble-Light-Preview" transform="translate(-180.000000, -7479.000000)" fill="#6B7280">
+                              <g id="icons" transform="translate(56.000000, 160.000000)">
+                                  <path d="M144,7339 L140,7339 L140,7332.001 C140,7330.081 139.153,7329.01 137.634,7329.01 C135.981,7329.01 135,7330.126 135,7332.001 L135,7339 L131,7339 L131,7326 L135,7326 L135,7327.462 C135,7327.462 136.255,7325.26 139.083,7325.26 C141.912,7325.26 144,7326.986 144,7330.558 L144,7339 L144,7339 Z M126.442,7323.921 C125.093,7323.921 124,7322.819 124,7321.46 C124,7320.102 125.093,7319 126.442,7319 C127.79,7319 128.883,7320.102 128.883,7321.46 C128.884,7322.819 127.79,7323.921 126.442,7323.921 L126.442,7323.921 Z M124,7339 L129,7339 L129,7326 L124,7326 L124,7339 Z" id="linkedin-[#161]"></path>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                </a>
+              </li>
+          </ul>
+          <div class="w-full">
+            <ul class="flex flex-col items-start space-y-3 font-light">
+              <li class="flex flex-row space-x-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>
+                <span class="text-xs text-gray-500">30 April 1997</span>
+              </li>
+              <li class="flex flex-row space-x-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+                <span class="text-xs text-gray-500">Pamekasan, Indonesia</span>
+              </li>
+              <li class="flex flex-row space-x-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                </svg>
+                <span class="text-xs text-gray-500">rizal.bachtiar.31@gmail.com</span>
+              </li>
+              <li class="flex flex-row space-x-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
+                </svg>
+                <span class="text-xs text-gray-500">+62 82 337 307 996</span>
+              </li>
+              <li class="flex flex-row space-x-1">
+                <svg class="h-4 w-4 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />  <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" /></svg>
+                <span class="text-xs text-gray-500">@rizalbachtiar.12</span>
+              </li>
+            </ul>
+          </div>
+          <button class="bg-blue-600 px-6 py-2 rounded-2xl text-gray-200 shadow-lg font-extralight flex items-center hover:bg-blue-500 transition">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+            </svg>
+            Download CV
+          </button>
         </div>
       </div>
     </div>
